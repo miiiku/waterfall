b4724e3a969db8fdabbf34cca75f2f8f499c39b9
miiiku <guanquanhong@163.com>
2018-08-13 16:57:19 +0800

    first submit

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const babel     = require('gulp-babel')
 const uglify    = require('gulp-uglify')
 const rename    = require('gulp-rename')
 
-gulp.task('compressJS', function () {
+gulp.task('compressJS', done => {
     gulp.src(['src/*.js'])
         .pipe(babel({
             presets: ['es2015']
@@ -13,12 +13,14 @@ gulp.task('compressJS', function () {
             suffix: ".min"
         }))
         .pipe(gulp.dest('dist'))
+
+    done()
 })
 
 gulp.task('watch', function() {
-    gulp.watch(['src/*.js'], ['compressJS'])
+    gulp.watch(['src/*.js'], gulp.series('compressJS'))
 })
 
-gulp.task('release', ['compressJS'])
+gulp.task('release', gulp.series('compressJS'))
 
-gulp.task('default', ['release', 'watch'])
+gulp.task('default', gulp.series(gulp.parallel('release', 'watch')))
